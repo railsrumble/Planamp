e418cda8efd5a96a00dece5db879cf56e571d524
sld <sld7700@gmail.com>
2014-10-18 15:55:27 +0400

    Maybe last fix of auth.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,25 +1,25 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    sign_in(:github)
+    oauth_sign_in(:github)
   end
 
   def twitter
-    sign_in(:twitter)
+    oauth_sign_in(:twitter)
   end
 
 
   protected
 
 
-  def sign_in(provider)
+  def oauth_sign_in(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      set_flash_message(:notice, :success, :kind => provider) if is_navigational_format?
-      sign_in_and_redirect @user, :event => :authentication and return
+      set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
+      sign_in_and_redirect(@user, event: :authentication) and return
     else
       flash[:error] = "Error in auth"
-      redirect_to root_url and return
+      redirect_to(root_url) and return
     end
   end
 
